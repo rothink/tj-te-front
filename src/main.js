b934a662243be99aa97cf35b6fe1b95af79926ac
Rodrigo Araujo <rodrigoluz@live.com>
2024-01-12 16:41:29 -0300

    package json, main and app

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import router from './router'
 
 // Components
 import App from './App.vue'
@@ -16,5 +17,6 @@ import { createApp } from 'vue'
 const app = createApp(App)
 
 registerPlugins(app)
+app.use(router)
 
 app.mount('#app')
